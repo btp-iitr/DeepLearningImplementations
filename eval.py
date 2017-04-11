3e1131a33c5999172700384bf13b45bc6c28bde4
Chetanya Rastogi <rastogi.chetanya19@gmail.com>
2017-04-11 22:38:00 +0530

    Updated eval.py

--- eval.py
+++ eval.py
@@ -3,18 +3,20 @@ from math import log2
 wb = open_workbook('data.xlsx')   #file containing the data
 r_score = []
 f_score = []
+ref = [1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0]
 for s in wb.sheets():
-    rs = 0
-    fs = 0
     for i in range(1, s.nrows):
-        p = s.cell(i, 1).value
-        if p <= 0.05:
-            p = 0.05        #lower and upper limit on the probability to avoid a loss of -INF
-        elif p >= 0.95:
-            p = 0.95
-        if s.cell(i, 0).value == 0:
-            fs += log2(2*p)
-        else:
-            rs += log2(2*p)
-    r_score.append(rs)
-    f_score.append(fs)
+        rs = 0
+        fs = 0
+        for j in range(1, s.ncols):
+            p = s.cell(i, j).value
+            if p <= 0.05:
+                p = 0.05        #lower and upper limit on the probability to avoid a loss of -INF
+            elif p >= 0.95:
+                p = 0.95
+            if ref[j-1] == 0:
+                fs += log2(2*p)
+            else:
+                rs += log2(2*p)
+        r_score.append(rs)
+        f_score.append(fs)
